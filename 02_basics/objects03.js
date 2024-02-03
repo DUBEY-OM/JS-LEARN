0372bc99cbecee58fe232ce2342e454c11002ea5
DUBEY-OM <158318737+DUBEY-OM@users.noreply.github.com>
2024-02-03 12:48:03 +0000

    objects and its use

--- 02_basics/objects03.js
+++ 02_basics/objects03.js
@@ -2,3 +2,5 @@
 const user={name:"Om",age:20,location:"cdg",email:"omjjj@gamil.com",isLoggedIn:false,lastlogindays:["monday","tuesday"]}
 console.log(user.lastlogindays)
 console.log(user["lastlogindays"])
+console.log(user["name"])
+console.log(typeof user["name"])
