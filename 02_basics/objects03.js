6711789ad0fec80e6447f9822f97275c14cfcb40
DUBEY-OM <158318737+DUBEY-OM@users.noreply.github.com>
2024-02-03 12:31:51 +0000

    objects

---
+++ 02_basics/objects03.js
@@ -0,0 +1,4 @@
+//object literal
+const user={name:"Om",age:20,location:"cdg",email:"omjjj@gamil.com",isLoggedIn:false,lastlogindays:["monday","tuesday"]}
+console.log(user.lastlogindays)
+console.log(user["lastlogindays"])
