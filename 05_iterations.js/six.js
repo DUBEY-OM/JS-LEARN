35e99ae276ddfd1ca84a11a2d3a18e1afa36cec9
DUBEY-OM <158318737+DUBEY-OM@users.noreply.github.com>
2024-02-18 17:35:05 +0000

    loops of higher order

---
+++ 05_iterations.js/six.js
@@ -0,0 +1,7 @@
+const coding = ["java", "python", "cpp", "ruby", "js"]
+const values = coding.forEach((element) => {
+    console.log(element);
+    return element;
+});
+console.log(values)//doesnot returns anything
+cont
