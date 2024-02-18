bd18081102bb308d51fa2f33ab9ebd810f1305c5
DUBEY-OM <158318737+DUBEY-OM@users.noreply.github.com>
2024-02-18 17:26:07 +0000

    higher oeder array loops

---
+++ 05_iterations.js/five.js
@@ -0,0 +1,15 @@
+const coding=["java","python","cpp","ruby","js"]
+coding.forEach( function (val){console.log(val);})
+coding.forEach((item,index,arr)=> {console.log(item,index,arr)
+    
+});
+const mycoding=[
+    {lang:"javascript",file:"js"},
+
+    {lang:"python",file:"py"},
+
+    {lang:"c++",file:"cpp"}
+]
+mycoding.forEach((element) => {console.log(element.lang)
+    
+});
